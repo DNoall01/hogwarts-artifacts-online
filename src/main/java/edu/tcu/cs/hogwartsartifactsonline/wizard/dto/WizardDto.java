8f069c66875d6d39e1152cba9aa3df10ef2c4a9d
Dylan Noall <dylannoall@outlook.com>
2025-03-23 23:11:42 -0500

    wizard CRUD complete

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/wizard/dto/WizardDto.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/wizard/dto/WizardDto.java
@@ -1,6 +1,10 @@
 package edu.tcu.cs.hogwartsartifactsonline.wizard.dto;
 
-public record WizardDto(Integer id, String name, Integer numberOfArtifacts) {
+import jakarta.validation.constraints.NotEmpty;
+
+public record WizardDto(Integer id,
+                        String name,
+                        Integer numberOfArtifacts) {
 
 
 }
